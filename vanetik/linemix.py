d940e4f196aa284c3d36931530f360c222d270a8
lucyhorowitz <111657652+lucyhorowitz@users.noreply.github.com>
2024-03-18 17:19:42 -0400

    fix wolframall_mix.csv

--- vanetik/linemix.py
+++ vanetik/linemix.py
@@ -1,16 +1,16 @@
 import random
 
 with open("wolframall_bad.txt", 'r') as file:
-    lines1 = [line.strip() for line in file.readlines()]
+    lines1 = ["\"" + line.strip() + "\",0" for line in file.readlines()]
 with open("wolframall_good.txt", 'r') as file:
-    lines2 = [line.strip() for line in file.readlines()]
+    lines2 = ["\"" + line.strip() + "\",1" for line in file.readlines()]
 
 lines = lines1 + lines2
 # Shuffle the lines randomly
 random.shuffle(lines)
 
 # Write shuffled lines back to the file
-with open("wolframall_mix.txt", 'w') as file:
+with open("wolframall_mix.csv", 'w') as file:
     file.writelines(line + '\n' for line in lines)
 
 
